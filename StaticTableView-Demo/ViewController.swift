86972157c9f74dfdcb050add46c8d56a4ea65e1f
Roberto Fernandes <roberto.fernandes21@gmail.com>
2020-10-20 11:54:41 +0100

    Initial Commit

---
+++ StaticTableView-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticTableView-Demo
+//
+//  Created by Roberto Fernandes on 20/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
